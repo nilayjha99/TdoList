d3959e63ee4371aa11e2561f8e0b2a53f5ecc072
Administrator <nhj123@udml11.local>
2018-10-22 21:17:02 -0600

    add: "immediate" and "unspecified" to date field. add: "done" to priority

--- TdoList/ViewController.swift
+++ TdoList/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var dueDateField: UITextField!
     private var datePicker : UIDatePicker?
     private var picker: UIPickerView?
+    private var selectedPrioriy: String = "low"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,35 +24,73 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    private func initDatePicker() {
+        // code for toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // add done button
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(dueDateDoneTapped))
+        
+        // add Immediate button
+        let immediatButton = UIBarButtonItem(title: "Immediate", style: .done, target: nil, action: #selector(dueDateImmediateTapped))
+
+        // add unspecify Button
+        let unspecifyButton = UIBarButtonItem(title: "Unspecified", style: .done, target: nil, action: #selector(dueDateUnspecifyTapped))
+
+        toolbar.setItems([doneButton, immediatButton, unspecifyButton], animated: true)
+        
+        //add toolbar to date picker
+        self.dueDateField.inputAccessoryView = toolbar
+        
+        // code to handle the duedate date input
+        self.datePicker =  UIDatePicker()
+        self.datePicker?.datePickerMode = .dateAndTime
+
+        // set datepicker as input control
+        self.dueDateField.inputView = self.datePicker
+    }
+    
+    @objc private func dueDateDoneTapped() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        self.dueDateField.text = dateFormatter.string(from: (self.datePicker?.date)!)
+        view.endEditing(true)
+    }
+    
+    @objc private func dueDateImmediateTapped() {
+        self.dueDateField.text = "Immediate"
+        view.endEditing(true)
+    }
+    
+    @objc private func dueDateUnspecifyTapped() {
+        self.dueDateField.text = "Unspecified"
+        view.endEditing(true)
+    }
+    
+    
+    
+ 
+    
     private func initPriorityPicker() {
+        // code for toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // add done button
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(priorityPickerDoneTapped))
+        toolbar.setItems([doneButton], animated: true)
         // code to handle priority input
         self.picker = UIPickerView()
         self.picker?.delegate = self
         self.picker?.dataSource = self
         self.priorityField.inputView = self.picker
+        self.priorityField.inputAccessoryView = toolbar
     }
     
-    private func initDatePicker() {
-        // code to handle the duedate date input
-        self.datePicker =  UIDatePicker()
-        self.datePicker?.datePickerMode = .dateAndTime
-        self.datePicker?.addTarget(self, action: #selector(ViewController.dateChanged(datePicker:)), for: .valueChanged)
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.viewTapped(gestureRecognizer:)))
-        view.addGestureRecognizer(tapGesture)
-        
-        self.dueDateField.inputView = self.datePicker
-    }
-    
-    @objc private func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
-        view.endEditing(true)
-    }
-    
-    @objc private func dateChanged(datePicker: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .short
-        self.dueDateField.text = dateFormatter.string(from: (self.datePicker?.date)!)
+    @objc private func priorityPickerDoneTapped() {
+        self.priorityField.text = self.selectedPrioriy
         view.endEditing(true)
     }
     
@@ -69,8 +109,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     // returns the selected input and sets it into the textfield
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.priorityField.text =  self.priorities[row]
-        view.endEditing(true)
+        self.selectedPrioriy =  self.priorities[row]
     }
     
     // set the appereance of the "lables" on the picker
