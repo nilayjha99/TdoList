d0a4763658179b1bb901be73964e02df90fcd203
Administrator <nhj123@udml11.local>
2018-10-25 20:36:30 -0600

    wip: model class

--- TdoList/TaskModel.swift
+++ TdoList/TaskModel.swift
@@ -7,26 +7,35 @@
 //
 import UIKit
 
-public enum dueDateEnum {
-    case unspecified(String?)
-    case specified(Date)
-    case immedeate(String)
-}
-
 class TaskModel {
    
-    var title: String?
-    var dateCreated: Date?
-    var dueDate: dueDateEnum?
+    var title: String
+    var dateCreated: String?
+    var dueDate: String?
+    var priority: String?
     var photo: UIImage?
-    var originalPhoto: UIImage?
+    var thumbnail: UIImage?
+    var imageFrameOffset: CGPoint?
     var notes: String?
     
-    init(title: String, dateCreated: Date, dueDate: dueDateEnum, photo: UIImage, notes: String) {
+    init(title: String, dateCreated: String?, dueDate: String?,
+         priority: String?,photo: UIImage?, thumbnail: UIImage?,
+         imageFrameOffset: CGPoint?, notes: String?) {
         self.title = title
         self.dateCreated = dateCreated
         self.dueDate = dueDate
         self.photo = photo
+        self.thumbnail = thumbnail
+        self.imageFrameOffset = imageFrameOffset
         self.notes = notes
     }
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    func getDateFromString(_ dateString: String) {
+        
+    }
+    
 }
